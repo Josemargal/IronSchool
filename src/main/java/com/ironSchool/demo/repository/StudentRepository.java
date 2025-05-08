805b76882efff88d41a7ab4ab4e40340714c35ac
Josemargal <126793937+Josemargal@users.noreply.github.com>
2025-05-08 16:21:56 +0200

    subida repository

--- src/main/java/com/ironSchool/demo/repository/StudentRepository.java
+++ src/main/java/com/ironSchool/demo/repository/StudentRepository.java
@@ -1,4 +1,8 @@
 package com.ironSchool.demo.repository;
 
-public class StudentRepository {
+
+import com.ironSchool.demo.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
