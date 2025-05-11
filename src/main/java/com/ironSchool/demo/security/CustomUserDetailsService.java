e889ef25467e8668b4df1d3b870fbcb9ba646103
Josemargal <126793937+Josemargal@users.noreply.github.com>
2025-05-11 19:22:02 +0200

    sbuida security

---
+++ src/main/java/com/ironSchool/demo/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.ironSchool.demo.security;
+
+import com.ironSchool.demo.model.Teacher;
+import com.ironSchool.demo.repository.TeacherRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final TeacherRepository teacherRepository;
+
+    public CustomUserDetailsService(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Teacher teacher = teacherRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return User.builder()
+                .username(teacher.getUsername())
+                .password(teacher.getPassword())
+                .roles("TEACHER")
+                .build();
+    }
+}
